71ff9e653c8b02647f5e7a4a1746681b1c597fdf
kshatriyaindraja <kshatriyaindraja@gmail.com>
2016-08-15 23:42:09 +0530

    json beautification including files with reduced number of lines

--- Program.cs
+++ Program.cs
@@ -14,68 +14,41 @@ namespace JSON
             string beautify = "";
             try
             {
-                // Create an instance of StreamReader to read from a file.
-                // The using statement also closes the StreamReader.
                 using (StreamReader sr = new StreamReader("C:/Users/Kshatriya Indraja/Documents/Visual Studio 2012/Projects/JSON/JSON/unbeautify.txt"))
                 {
                     string line;
-
-                    // Read and display lines from the file until 
-                    // the end of the file is reached. 
                     while ((line = sr.ReadLine()) != null)
                     {
                         beautify = string.Concat(beautify, line);
-                        //Console.WriteLine(line);
                     }
                     Console.WriteLine(beautify);
                 }
             }
             catch (Exception e)
             {
-
-                // Let the user know what went wrong.
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
             int count = 0;
-            //Console.WriteLine(str);
-            //foreach (char c in beautify)
-            //{
-            //    //Console.WriteLine(str);
-                
-
-
-            //}
-
             using (StreamWriter sw = new StreamWriter("C:/Users/Kshatriya Indraja/Documents/Visual Studio 2012/Projects/JSON/JSON/beautify.txt"))
             {
-
                 foreach (char s in beautify)
                 {
-                   // sw.WriteLine(s);
                     switch (s)
                     {
                         case '{':
                             {
                                 sw.WriteLine("{" + "\n");
                                 count++;
-                                //Console.Write(count);
-
                                 for (int i = 0; i < count; i++)
-                                {
                                     sw.Write("\t");
-                                }
-
-                                // Console.WriteLine("\n");
                                 continue;
                             }
                         case ',':
                             {
                                 sw.WriteLine("," + "\n");
                                 for (int i = 0; i < count; i++)
-                                {
                                     sw.Write("\t");
-                                }
                                 continue;
                             }
                         case ':':
@@ -85,25 +58,18 @@ namespace JSON
                             }
                         case '[':
                             {
-                                // Console.WriteLine("hai");
                                 sw.WriteLine("[" + "\n");
                                 count++;
-                                // Console.Write(count);
                                 for (int i = 0; i < count; i++)
-                                {
                                     sw.Write("\t");
-                                }
                                 continue;
                             }
                         case ']':
                             {
                                 sw.WriteLine("\n");
                                 count--;
-                                // Console.Write(count);
                                 for (int i = 0; i < count; i++)
-                                {
                                     sw.Write("\t");
-                                }
                                 sw.Write("]");
                                 continue;
                             }
@@ -111,40 +77,30 @@ namespace JSON
                             {
                                 sw.WriteLine("\n");
                                 count--;
-                                //  Console.Write(count);
                                 for (int i = 0; i < count; i++)
-                                {
-                                    sw.Write("\t");
-                                }
+                                   sw.Write("\t");
                                 sw.Write("}");
                                 continue;
                             }
                         case ' ':
                             {
                                 sw.Write(s);
-                                continue;
-
+                                break;
                             }
                         default:
                             {
                                 sw.Write(s);
-
                                 break;
                             }
                     }
                 }
             }
-
-            // Read and show each line from the file.
             string loadline = "";
             using (StreamReader sr = new StreamReader("beautify.txt"))
             {
                 while ((loadline = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine(loadline);
-                }
+                  Console.WriteLine(loadline);
             }
-
             Console.ReadKey();
         }
     }
